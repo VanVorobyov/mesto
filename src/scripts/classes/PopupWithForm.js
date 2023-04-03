c06e6420c29ac04ec65ce6f8a4daea7f855741e2
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-04-03 21:45:45 +0300

    refactor: add js classes, use webpack

---
+++ src/scripts/classes/PopupWithForm.js
@@ -0,0 +1,36 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleSubmitForm) {
+    super(popupSelector);
+    this._handleSubmitForm = handleSubmitForm;
+    this._form = this._popup.querySelector('.popup__form');
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    this._submitButton = this._form.querySelector('.popup__button');
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+  }
+
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach((input) => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
+  }
+
+  _handleFormSubmit(evt) {
+    evt.preventDefault();
+    this._handleSubmitForm(this._getInputValues());
+    this.close();
+  }
+
+  close() {
+    this._form.reset();
+    super.close();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', this._handleFormSubmit);
+  }
+}
---
+++ src/scripts/index.js
@@ -0,0 +1,91 @@
+// Импортирование классов и констант
+import '../pages/index.css';
+import Card from '../scripts/classes/Card.js';
+import FormValidator from '../scripts/classes/FormValidator.js';
+import {
+  validationOptions,
+  popupElementsCommon,
+  popupElementsUser,
+  popupElementsAddCard,
+  popupElementsFullImage,
+  cardConfig,
+} from './utils/constants.js';
+import { initialCards } from './utils/cards.js';
+import Section from '../scripts/classes/Section.js';
+import Popup from '../scripts/classes/Popup.js';
+import PopupWithForm from '../scripts/classes/PopupWithForm.js';
+import PopupWithImage from '../scripts/classes/PopupWithImage.js';
+import UserInfo from '../scripts/classes/UserInfo.js';
+
+// Валидация
+const formValidators = {};
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(validationOptions.formSelector));
+  formList.forEach((formElement) => {
+    const validator = new FormValidator(config, formElement);
+    const formName = formElement.getAttribute('name');
+    formValidators[formName] = validator;
+    validator.enableValidation();
+  });
+};
+
+const openPopupWithImage = (item) => {
+  const popupWithImage = new PopupWithImage('.popup_type_opened-image');
+  popupWithImage.open(item);
+  popupWithImage.setEventListeners();
+};
+
+const createCard = (cardData) => {
+  const card = new Card(cardData, cardConfig, openPopupWithImage);
+  const cardElement = card.generateCard();
+  return cardElement;
+};
+
+// Рендер всех карточек
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      cardList.addItem(createCard(item));
+    },
+  },
+  popupElementsCommon.cards
+);
+
+cardList.renderItems();
+
+// Попап пользователя
+const userInfo = new UserInfo({
+  userNameSelector: popupElementsUser.usernameSelector,
+  userAboutSelector: popupElementsUser.userAboutSelector,
+});
+// Открывает попап редактирования профиля
+const openPopupEditProfile = ({ name, about }) => {
+  popupElementsUser.inputUserName.value = name;
+  popupElementsUser.inputUserAbout.value = about;
+  formValidators['user-form'].resetValidation();
+  popupEditUserInfo.open();
+};
+// Обрабатывает информация профиля
+const popupEditUserInfo = new PopupWithForm(popupElementsUser.popupUserInfo, (data) => {
+  userInfo.setUserInfo(data);
+  popupEditUserInfo.close();
+});
+popupEditUserInfo.setEventListeners();
+
+popupElementsUser.buttonEditProfile.addEventListener('click', () => {
+  openPopupEditProfile(userInfo.getUserInfo());
+});
+
+// Попап добавления карточки
+const popupAddImage = new PopupWithForm(popupElementsAddCard.popupAddCard, (cardData) => {
+  cardList.addItem(createCard(cardData));
+});
+popupAddImage.setEventListeners();
+
+popupElementsAddCard.buttonAddCard.addEventListener('click', () => {
+  formValidators['card-form'].resetValidation();
+  popupAddImage.open();
+});
+
+enableValidation(validationOptions);
